fde001bb72fc9c9fa904bfe7536ad46e8911b963
Waren Gonzaga <emiren.warengonzaga.official@gmail.com>
2016-09-15 14:05:21 +0000

    Initial Commit

---
+++ dev/animateCSSPlugin.js
@@ -0,0 +1,9 @@
+/**
+ * This is script
+ */
+ 
+document.getElementsByTagName("head")[0].innerHTML="<script src='blabla'></script>";
+ 
+function bounce() {
+    console.log("Initiating Bounce");
+}
