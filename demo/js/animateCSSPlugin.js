3bd45005129a953d65c1bbe92627daedb789160a
Waren Gonzaga <emiren.warengonzaga.official@gmail.com>
2016-09-26 14:10:58 +0000

    Update the files

--- demo/js/animateCSSPlugin.js
+++ demo/js/animateCSSPlugin.js
@@ -1,1 +1,79 @@
-"use-strict";function bounce(){console.log("Bounce!"),TweenMax.to(this.target,mediumSpeed,{y:"-=30px"}),TweenMax.to(this.target,normalSpeed,{delay:.2,y:0,ease:Bounce.easeOut})}function flash(){console.log("Flash!"),TweenMax.to(this.target,mediumSpeed,{alpha:0}),TweenMax.to(this.target,mediumSpeed,{delay:.3,alpha:1}),TweenMax.to(this.target,mediumSpeed,{delay:.6,alpha:0}),TweenMax.to(this.target,mediumSpeed,{delay:.9,alpha:1})}function pulse(){console.log("Pulse!"),TweenMax.to(this.target,normalSpeed,{scale:1.1}),TweenMax.to(this.target,normalSpeed,{delay:.3,scale:1})}function rubberBand(){console.log("Rubber Band!"),TweenMax.to(this.target,.2,{scaleX:1.2,scaleY:.8}),TweenMax.to(this.target,.8,{scaleX:1,scaleY:1,delay:mediumSpeed,ease:Elastic.easeOut})}function shake(){TweenMax.to(this.target,normalSpeed,{x:"-=10px"}),TweenMax.to(this.target,normalSpeed,{x:"+=40px",delay:highSpeed}),TweenMax.to(this.target,normalSpeed,{x:"-=40px",delay:2*highSpeed}),TweenMax.to(this.target,normalSpeed,{x:"+=40px",delay:3*highSpeed}),TweenMax.to(this.target,normalSpeed,{x:"-=40px",delay:4*highSpeed}),TweenMax.to(this.target,normalSpeed,{x:"+=40px",delay:5*highSpeed}),TweenMax.to(this.target,normalSpeed,{x:"-=40px",delay:6*highSpeed}),TweenMax.to(this.target,normalSpeed,{x:"+=40px",delay:7*highSpeed}),TweenMax.to(this.target,normalSpeed,{x:"-=40px",delay:8*highSpeed}),TweenMax.to(this.target,normalSpeed,{x:0,delay:9*highSpeed})}function swing(){TweenMax.to(this.target,mediumSpeed,{rotationZ:"+=10"}),TweenMax.to(this.target,mediumSpeed,{rotationZ:"-=20",delay:mediumSpeed}),TweenMax.to(this.target,mediumSpeed,{rotationZ:"+=15",delay:2*mediumSpeed}),TweenMax.to(this.target,mediumSpeed,{rotationZ:"-=15",delay:3*mediumSpeed}),TweenMax.to(this.target,mediumSpeed,{rotationZ:0,delay:4*mediumSpeed})}function tada(){TweenMax.to(this.target,mediumSpeed,{rotationZ:"+=10",scale:.9}),TweenMax.to(this.target,mediumSpeed,{rotationZ:"-=20",scale:1.1,delay:highSpeed}),TweenMax.to(this.target,mediumSpeed,{rotationZ:"+=20",delay:2*highSpeed}),TweenMax.to(this.target,mediumSpeed,{rotationZ:"-=20",delay:3*highSpeed}),TweenMax.to(this.target,mediumSpeed,{rotationZ:"+=20",delay:4*highSpeed}),TweenMax.to(this.target,mediumSpeed,{rotationZ:"-=20",delay:5*highSpeed}),TweenMax.to(this.target,mediumSpeed,{rotationZ:0,scale:1,delay:6*highSpeed})}var highSpeed=.1,midHighSpeed=.2,mediumSpeed=.3,normalSpeed=.5;
+/**
+ * This is script
+ */
+
+
+/*
+*
+* var TweenMaxCDN = "<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js'></script>";
+* document.write(TweenMaxCDN);
+* 
+*/
+
+"use-strict";
+
+var highSpeed = 0.1,
+    midHighSpeed = 0.2,
+    mediumSpeed = 0.3,
+    normalSpeed = 0.5;
+ 
+function bounce() {
+    console.log("Bounce!");
+    TweenMax.to(this.target, mediumSpeed, {y: "-=30px"});
+    TweenMax.to(this.target, normalSpeed, {delay: 0.2, y: 0, ease:Bounce.easeOut});
+}
+
+function flash() {
+    console.log("Flash!");
+    TweenMax.to(this.target, mediumSpeed, {alpha: 0});
+    TweenMax.to(this.target, mediumSpeed, {delay: 0.3, alpha: 1});
+    TweenMax.to(this.target, mediumSpeed, {delay: 0.6, alpha: 0});
+    TweenMax.to(this.target, mediumSpeed, {delay: 0.9, alpha: 1});
+}
+
+function pulse() {
+    console.log("Pulse!");
+    TweenMax.to(this.target, normalSpeed, {scale: 1.1});
+    TweenMax.to(this.target, normalSpeed, {delay: 0.3, scale: 1});
+}
+
+function rubberBand() {
+    console.log("Rubber Band!");
+    TweenMax.to(this.target, 0.2, {scaleX: 1.2, scaleY: 0.8});
+    TweenMax.to(this.target, 0.8, {scaleX: 1, scaleY: 1, delay: mediumSpeed, ease:Elastic.easeOut});
+}
+
+function shake() {
+    TweenMax.to(this.target, normalSpeed, {x: "-=10px"});
+    TweenMax.to(this.target, normalSpeed, {x: "+=40px", delay: highSpeed});
+    TweenMax.to(this.target, normalSpeed, {x: "-=40px", delay: highSpeed * 2});
+    TweenMax.to(this.target, normalSpeed, {x: "+=40px", delay: highSpeed * 3});
+    TweenMax.to(this.target, normalSpeed, {x: "-=40px", delay: highSpeed * 4});
+    TweenMax.to(this.target, normalSpeed, {x: "+=40px", delay: highSpeed * 5});
+    TweenMax.to(this.target, normalSpeed, {x: "-=40px", delay: highSpeed * 6});
+    TweenMax.to(this.target, normalSpeed, {x: "+=40px", delay: highSpeed * 7});
+    TweenMax.to(this.target, normalSpeed, {x: "-=40px", delay: highSpeed * 8});
+    TweenMax.to(this.target, normalSpeed, {x: 0, delay: highSpeed * 9});
+}
+
+function swing() {
+    TweenMax.to(this.target, mediumSpeed, {rotationZ: "+=10"});
+    TweenMax.to(this.target, mediumSpeed, {rotationZ: "-=20", delay: mediumSpeed});
+    TweenMax.to(this.target, mediumSpeed, {rotationZ: "+=15", delay: mediumSpeed * 2});
+    TweenMax.to(this.target, mediumSpeed, {rotationZ: "-=15", delay: mediumSpeed * 3});
+    TweenMax.to(this.target, mediumSpeed, {rotationZ: 0, delay: mediumSpeed * 4});
+}
+
+function tada() {
+    TweenMax.to(this.target, mediumSpeed, {rotationZ: "+=10", scale: 0.9});
+    TweenMax.to(this.target, mediumSpeed, {rotationZ: "-=20", scale: 1.1, delay: highSpeed});
+    TweenMax.to(this.target, mediumSpeed, {rotationZ: "+=20", delay: highSpeed * 2});
+    TweenMax.to(this.target, mediumSpeed, {rotationZ: "-=20", delay: highSpeed * 3});
+    TweenMax.to(this.target, mediumSpeed, {rotationZ: "+=20", delay: highSpeed * 4});
+    TweenMax.to(this.target, mediumSpeed, {rotationZ: "-=20", delay: highSpeed * 5});
+    TweenMax.to(this.target, mediumSpeed, {rotationZ: 0, scale: 1, delay: highSpeed * 6});
+}
+
+function sample() {
+    alert('sample!');
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 /* File: gulpfile.js */
 
-// grab our gulp packages
+/* Gulp Packages */
 var gulp  = require('gulp'),
     gutil = require('gulp-util'),
     jade = require('gulp-jade'),
@@ -8,72 +8,116 @@ var gulp  = require('gulp'),
     sassLint = require('gulp-sass-lint'),
     jshint = require('gulp-jshint'),
     minify = require('gulp-minifier'),
-    multiDest = require('gulp-multi-dest');
+    multiDest = require('gulp-multi-dest'),
+    rename = require('gulp-rename');
     
 var destOptions = {
     mode: 0755
 };
 
-gulp.task('default', function() {
-  gutil.log(gutil.colors.green('Starting...'))
-  gutil.log(gutil.colors.green('Writen by Waren Gonzaga'))
+/**
+ * Gulp Tasks
+ * Writen by Waren Gonzaga
+ */
+
+gulp.task('default', ['jade','sass','jshint','build'], function() {
+  gutil.log(gutil.colors.green('Starting...'));
+  gutil.log(gutil.colors.green('Writen by Waren Gonzaga'));
   gulp.watch('source/*jade', ['jade'], gutil.log(
     gutil.colors.cyan('Watching index.jade file...'),
     gutil.colors.green('OK!')
-    ))
+    ));
   gulp.watch('source/sass/**/*.scss', ['sass'], gutil.log(
     gutil.colors.cyan('Watching sass files...'),
     gutil.colors.green('OK!')
-    ))
+    ));
   gulp.watch('source/javascript/**/*.js', ['jshint'], gutil.log(
     gutil.colors.cyan('Watching javascript files...'),
     gutil.colors.green('OK!')
-    ))
-  gutil.log(gutil.colors.cyan('Watching Changes...'))
+    ));
+  gutil.log(gutil.colors.cyan('Watching Changes...'));
 });
 
 gulp.task('jade', function() {
-    gutil.log(gutil.colors.cyan('Compiling Jade file...'))
-    gutil.log(gutil.colors.cyan('Updating HTML...'))
-    gutil.log(gutil.colors.cyan('Status: '), gutil.colors.green('DONE!'))
-    gulp.src('source/*.jade')
-    .pipe(jade({
-      pretty: true
-    }))
-  .pipe(gulp.dest('demo'))
-})
+  gutil.log(gutil.colors.cyan('Compiling Jade file...'));
+  gutil.log(gutil.colors.cyan('Updating HTML...'));
+  gutil.log(gutil.colors.cyan('Status: '), gutil.colors.green('DONE!'));
+  gutil.log(gutil.colors.cyan('Watching Changes...'));
+  gulp.src('source/*.jade')
+  .pipe(jade({
+    pretty: true
+  }))
+  .pipe(gulp.dest('demo'));
+});
 
 gulp.task('sass', function() {
-  gutil.log(gutil.colors.cyan('Compiling SCSS Files...'))
-  gutil.log(gutil.colors.cyan('Updating CSS File'))
-  gutil.log(gutil.colors.cyan('Status: '), gutil.colors.green('DONE!'))
+  gutil.log(gutil.colors.cyan('Compiling SCSS Files...'));
+  gutil.log(gutil.colors.cyan('Updating CSS File'));
+  gutil.log(gutil.colors.cyan('Status: '), gutil.colors.green('DONE!'));
+  gutil.log(gutil.colors.cyan('Watching Changes...'));
   gulp.src('source/sass/**/*.scss')
   .pipe(sassLint())
-  .pipe(sassLint.format())
   .pipe(sassLint.failOnError())
-  .pipe(sass.sync().on('error', sass.logError))
-  .pipe(gulp.dest('demo/css'))
-})
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(rename('demo/css/style.min.css'))
+  .pipe(gulp.dest(''));
+});
 
 gulp.task('jshint', function() {
-    gutil.log(gutil.colors.cyan('Updating JavaScript Files...'))
-    gutil.log(gutil.colors.cyan('Status: '), gutil.colors.green('DONE!'))
-    gulp.src('source/javascript/**/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(minify({
-      minify: true,
-      collapseWhitespace: true,
-      conservativeCollapse: true,
-      minifyJS: true,
-  })).pipe(multiDest(['build', 'demo/js'], destOptions));
-})
-
-gulp.task('production', function() {
-  return gulp.src('source/javascript/**/*.js').pipe(minify({
+  gutil.log(gutil.colors.cyan('Updating JavaScript Files...'));
+  gutil.log(gutil.colors.cyan('Status: '), gutil.colors.green('DONE!'));
+  gutil.log(gutil.colors.cyan('Watching Changes...'));
+  
+  /** Minified animateCSSplugin **/
+  gulp.src('source/javascript/animateCSSPlugin.js')
+  .pipe(jshint('.jshintrc'))
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(minify({
     minify: true,
     collapseWhitespace: true,
     conservativeCollapse: true,
     minifyJS: true,
-  })).pipe(multiDest(['build', 'demo/js'], destOptions));
+  }))
+  .pipe(rename('demo/js/animateCSSPlugin.min.js'))
+  .pipe(gulp.dest(''));
+    
+  /** Minified Script **/  
+  gulp.src('source/javascript/script.js')
+  .pipe(jshint('.jshintrc'))
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(minify({
+    minify: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    minifyJS: true,
+  }))
+  .pipe(rename('demo/js/script.min.js'))
+  .pipe(gulp.dest(''));
+});
+
+gulp.task('build', function() {
+  gutil.log(gutil.colors.cyan('Building and Updating your animateCSSplugin'));
+  gutil.log(gutil.colors.cyan('By Waren Gonzaga...'));
+  /** Minified build **/
+  gulp.src('source/javascript/animateCSSPlugin.js')
+  .pipe(jshint('.jshintrc'))
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(minify({
+    minify: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    minifyJS: true,
+  }))
+  .pipe(rename('build/animateCSSPlugin.min.js'))
+  .pipe(gulp.dest(''));
+  gutil.log(gutil.colors.cyan('Minified:'), gutil.colors.green('DONE!!!'));
+  
+  /** Production build **/
+  gulp.src('source/javascript/animateCSSPlugin.js')
+  .pipe(jshint('.jshintrc'))
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(multiDest(['build','demo/js'], destOptions));
+  gutil.log(gutil.colors.cyan('Production:'), gutil.colors.green('DONE!!!'));
+  
+  gutil.log(gutil.colors.cyan('Status:'), gutil.colors.green('DONE!!!'));
 });
