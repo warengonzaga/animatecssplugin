a427657479f913ef2b58d71c42b368afac779bd3
Waren Gonzaga <warengonzaga.dev@gmail.com>
2018-08-05 07:30:35 +0000

    Update the version of downloadables

--- animateCSSPlugin.js
+++ animateCSSPlugin.js
@@ -1,21 +1,28 @@
-/*
-*
-* var TweenMaxCDN = "<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js'></script>";
-* document.write(TweenMaxCDN);
-* 
-*/
-
-/* global TweenMax, Bounce, Elastic */
+/*!
+ * animatecssplugin - https://github.com/WarenGonzaga/AnimateCSSPlugin#readme
+ * Version: 0.3.0
+ * Demo: https://warengonzaga.github.io/sites/animatecssplugin.html
+ * Licensed under the MIT license - http://opensource.org/licenses/MIT
+ * Copyright (c) 2018 Waren Gonzaga
+ * 
+ * Facebook: @WarenGonzagaOfficialPage
+ * Twitter: @Waren_Gonzaga
+ * Github: @WarenGonzaga
+ */
 
 "use-strict";
 
 var highSpeed = 0.1,
     midHighSpeed = 0.2,
     mediumSpeed = 0.3,
-    normalSpeed = 0.5;
+    normalSpeed = 0.5,
+    slowSpeed = 0.7,
+    oneSecSpeed = 1.0;
+
+var tl = new TimelineMax();
     
 function reset() {
-    TweenMax.set(this.target, {x: 0, y: 0, z: 0, scale: 1, rotation: 0, transformOrigin: "center center"});
+    TweenMax.set(this.target, {x: 0, y: 0, z: 0, opacity: 1, scale: 1, rotationX: 0 , rotationY: 0 , rotation: 0, transformOrigin: "center center"});
 }
  
 function bounce() {
@@ -40,8 +47,8 @@ function pulse() {
 
 function rubberBand() {
     console.log("Rubber Band!");
-    TweenMax.to(this.target, 0.2, {scaleX: 1.2, scaleY: 0.8});
-    TweenMax.to(this.target, 0.8, {scaleX: 1, scaleY: 1, delay: mediumSpeed, ease:Elastic.easeOut, onComplete: reset});
+    TweenMax.to(this.target, midHighSpeed, {scaleX: 1.2, scaleY: 0.8});
+    TweenMax.to(this.target, fastSpeed, {scaleX: 1, scaleY: 1, delay: mediumSpeed, ease:Elastic.easeOut, onComplete: reset});
 }
 
 function shake() {
@@ -76,3 +83,266 @@ function tada() {
     TweenMax.to(this.target, highSpeed, {rotation: "-=10", delay: midHighSpeed + (0.1 * 4)});
     TweenMax.to(this.target, highSpeed, {rotation: "+=5", scale: 1, delay: midHighSpeed + (0.1 * 5), onComplete: reset});
 }
+
+function wobble() {
+
+    tl.to(this.target, 0.13, {rotation: -3, x: -80 ,y: 4})
+    .to(this.target, 0.13, {rotation: 3, x: 80 ,y: -4})
+    .to(this.target, 0.13, {rotation: -3, x: -60 ,y: 3})
+    .to(this.target, 0.13, {rotation: 3, x: 60 ,y: -3})
+    .to(this.target, 0.13, {rotation: -3, x: -30 ,y: 2})
+    .to(this.target, 0.13, {rotation: 3, x: 30 ,y: -2})
+    .to(this.target, 0.13, {rotation: 0, x: -5 ,y: 0})
+    .to(this.target, 0.13, {rotation: 0, x: 0 ,y: 0});
+}
+
+function jello() {
+
+    tl.to(this.target, midHighSpeed, {skewX: -25 , skewY: -12.5 ,ease: Power1.easeOut})
+    .to(this.target, highSpeed, {skewX: 7 , skewY: 14 ,ease: Power1.easeOut})
+    .to(this.target, highSpeed, {skewX: -10 , skewY: -5 ,ease: Power1.easeOut})
+    .to(this.target, highSpeed, {skewX: 3 , skewY: 1.5 ,ease: Power1.easeOut})
+    .to(this.target, highSpeed, {skewX: -1 , skewY : -1, ease: Power1.easeOut , onComplete: reset});
+}
+
+
+function bounceIn() {
+
+    tl.to(this.target, highSpeed, {opacity: 0 , scale:0 })
+    .to(this.target, midHighSpeed, {opacity: 1 , scale: 1.08 })
+    .to(this.target, midHighSpeed, {opacity: 1 , scale: 0.8 })
+    .to(this.target, highSpeed, {opacity: 1 , scale: 0.95 , ease: Power1.easeOut })
+    .to(this.target, midHighSpeed, {opacity: 1 , scale: 1 , onComplete:reset });
+}
+
+function bounceInDown() {
+
+    tl.set(this.target, {y: -200 })
+    .to(this.target, midHighSpeed, {y: 0 })
+    .to(this.target, midHighSpeed, {y:-20 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {y: 0 })
+    .to(this.target, midHighSpeed, {y:-5 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {y: 0, onComplete:reset });
+}
+
+function bounceInLeft() {
+
+    tl.set(this.target, {x: -2000 })
+    .to(this.target, mediumSpeed, {x: 0 })
+    .to(this.target, midHighSpeed, {x:-20 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {x: 0 })
+    .to(this.target, midHighSpeed, {x:-5 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {x: 0, onComplete:reset });
+}
+
+function bounceInRight() {
+
+    tl.set(this.target, {x: 2000 })
+    .to(this.target, mediumSpeed, {x: 0 })
+    .to(this.target, midHighSpeed, {x:20 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {x: 0 })
+    .to(this.target, midHighSpeed, {x:5 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {x: 0, onComplete:reset });
+}
+
+function bounceInUp() {
+
+    tl.set(this.target, {y: 200 })
+    .to(this.target, midHighSpeed, {y: 0 })
+    .to(this.target, midHighSpeed, {y:20 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {y: 0 })
+    .to(this.target, midHighSpeed, {y:5 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {y: 0, onComplete:reset });
+}
+
+function bounceOut() {
+
+    tl.to(this.target, midHighSpeed, {opacity: 1 , scale: 1 })
+    .to(this.target, highSpeed, {opacity: 1 , scale: 0.95 , ease: Power1.easeOut })
+    .to(this.target, midHighSpeed, {opacity: 1 , scale: 0.8 })
+    .to(this.target, midHighSpeed, {opacity: 1 , scale: 1.08 })
+    .to(this.target, 0.4, {opacity: 0 , scale:0 , onComplete: reset });
+}
+
+function bounceOutDown() {
+
+    tl.set(this.target, {y: 0 })
+    .to(this.target, midHighSpeed, {y: 20 })
+    .to(this.target, midHighSpeed, {y: 0 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {y: 10 })
+    .to(this.target, midHighSpeed, {y: 0 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {y: 1000, onComplete:reset });
+}
+
+function bounceOutLeft() {
+
+    tl.set(this.target, {x: 0 })
+    .to(this.target, mediumSpeed, {x: -5 })
+    .to(this.target, midHighSpeed, {x:0, ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {x: -20 })
+    .to(this.target, midHighSpeed, {x:0 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {x: -2000, onComplete:reset });
+}
+
+function bounceOutRight() {
+
+    tl.set(this.target, {x: 0 })
+    .to(this.target, mediumSpeed, {x: 5 })
+    .to(this.target, midHighSpeed, {x:0, ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {x: 20 })
+    .to(this.target, midHighSpeed, {x:0 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {x: 2000, onComplete:reset });
+}
+
+function bounceOutUp() {
+
+    tl.set(this.target, {y: 0 })
+    .to(this.target, midHighSpeed, {y: -20 })
+    .to(this.target, midHighSpeed, {y: 0 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {y: -10 })
+    .to(this.target, midHighSpeed, {y: 0 , ease: Back.easeOut.config(1.7) })
+    .to(this.target, midHighSpeed, {y: -1000, onComplete:reset });
+}
+
+function fadeIn() {
+
+    tl.set(this.target, { opacity: 0 })
+    .to(this.target, oneSecSpeed, {opacity: 1 , ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeInDown() {
+
+    tl.set(this.target, { opacity: 0 , y: -200 })
+    .to(this.target, oneSecSpeed, {opacity: 1, y :0, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeInDownBig() {
+
+    tl.set(this.target, { opacity: 0 , y: -2000 })
+    .to(this.target, oneSecSpeed, {opacity: 1, y :0, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeInLeft() {
+
+    tl.set(this.target, { opacity: 0 , x: -500 })
+    .to(this.target, oneSecSpeed, {opacity: 1, x :0, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeInLeftBig() {
+
+    tl.set(this.target, { opacity: 0 , x: -2000 })
+    .to(this.target, oneSecSpeed, {opacity: 1, x :0, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeInRight() {
+
+    tl.set(this.target, { opacity: 0 , x: 500 })
+    .to(this.target, oneSecSpeed, {opacity: 1, x :0, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeInRightBig() {
+
+    tl.set(this.target, { opacity: 0 , x: 2000 })
+    .to(this.target, oneSecSpeed, {opacity: 1, x :0, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeInUp() {
+
+    tl.set(this.target, { opacity: 0 , y: 200 })
+    .to(this.target, oneSecSpeed, {opacity: 1, y :0, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeInUpBig() {
+
+    tl.set(this.target, { opacity: 0 , y: 2000 })
+    .to(this.target, oneSecSpeed, {opacity: 1, y :0, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeOut() {
+
+    tl.set(this.target, { opacity: 1 })
+    .to(this.target, oneSecSpeed, {opacity: 0 , ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeOutDown() {
+
+    tl.set(this.target, { opacity: 1 , y: 0 })
+    .to(this.target, oneSecSpeed, {opacity: 0, y :200, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeOutDownBig() {
+
+    tl.set(this.target, { opacity: 1 , y: 0 })
+    .to(this.target, oneSecSpeed, {opacity: 0, y :2000, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeOutLeft() {
+
+    tl.set(this.target, { opacity: 1 , x: 0 })
+    .to(this.target, oneSecSpeed, {opacity: 0, x :-500, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeOutLeftBig() {
+
+    tl.set(this.target, { opacity: 1 , x: 0 })
+    .to(this.target, oneSecSpeed, {opacity: 0, x :-2000, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeOutRight() {
+
+    tl.set(this.target, { opacity: 1 , x: 0 })
+    .to(this.target, oneSecSpeed, {opacity: 0, x :500, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeOutRightBig() {
+
+    tl.set(this.target, { opacity: 1 , x: 0 })
+    .to(this.target, oneSecSpeed, {opacity: 0, x :2000, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeOutUp() {
+
+    tl.set(this.target, { opacity: 1 , y: 0 })
+    .to(this.target, oneSecSpeed, {opacity: 0, y :-200, ease: Power1.easeOut, onComplete:reset });
+}
+
+function fadeOutUpBig() {
+
+    tl.set(this.target, { opacity: 1 , y: 0 })
+    .to(this.target, oneSecSpeed, {opacity: 0, y :-2000, ease: Power1.easeOut, onComplete:reset });
+}
+
+function flip() {
+
+    tl.to(this.target, slowSpeed, {rotationY : -180 , ease: Power1.easeOut})
+    .to(this.target, highSpeed, {scale : 1.5, ease: Power1.easeOut})
+    .to(this.target, midHighSpeed, {rotationY : -180 , scale : 1 , onComplete: reset});
+}
+
+function flipInX() {
+
+    tl.set(this.target, {rotationX : -90 , opacity: 0})
+    .to(this.target, 1.5, {rotationX : 0 , opacity: 1 , ease: Back.easeOut.config(4) , onComplete: reset});
+}
+
+function flipInY() {
+
+    tl.set(this.target, {rotationY : -120 , opacity: 0})
+    .to(this.target, 1.5, {rotationY : 0 , opacity: 1 , ease: Back.easeOut.config(3) , onComplete: reset});
+}
+
+function flipOutX() {
+
+    tl.set(this.target, {rotationX : 0 , opacity: 1})
+    .to(this.target, oneSecSpeed, {rotationX : 30 , opacity: 1 , ease: Back.easeOut.config(4)})
+    .to(this.target, oneSecSpeed, {rotationX : 90 , opacity: 0 , ease: Back.easeOut.config(4) , onComplete: reset});
+}
+
+function flipOutY() {
+
+    tl.set(this.target, {rotationY : 0 , opacity: 1})
+    .to(this.target, oneSecSpeed, {rotationY : -30 , opacity: 1 , ease: Back.easeOut.config(4)})
+    .to(this.target, oneSecSpeed, {rotationY : 90 , opacity: 0 , ease: Back.easeOut.config(4) , onComplete: reset});
+}
+
+
